69671ac9cd512be6d4ab86475e542d17a756f947
DKing92 <danielking92@gmail.com>
2019-03-04 20:20:16 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,37 +1,54 @@
 const celsius = document.querySelector('.celsius'),
-      farenheit = document.querySelector('.farenheit'),
-      results = document.querySelector('.results')
+       farenheit = document.querySelector('.farenheit'),
+       results = document.querySelector('.results'),
+       temperature = document.getElementById('temperature');
       
 
-celsius.addEventListener('click', function(e){
-    // Set the entered number to a variable
-    number = parseFloat(document.getElementById('temperature').value)
+// celsius.addEventListener('click', function(e){
+//     // Set the entered number to a variable
+//     number = parseFloat(document.getElementById('temperature').value)
     
-    //Check if number
-    if(isNaN(number)) {
-        alert('Please enter a valid number');
-    } else {
-    // Convert to Farenheit
-    const farenheitConverted = ((number-32)*5/9).toFixed(2);
-    results.innerHTML = `${farenheitConverted} °C`
-    }
-    document.getElementById('form').reset()
-   e.preventDefault()
-});
+//     //Check if number
+//     if(isNaN(number)) {
+//         alert('Please enter a valid number');
+//     } else {
+//     // Convert to Farenheit
+//     const celsiusConverted = ((number-32)*5/9).toFixed(2);
+//     results.innerHTML = `${celsiusConverted} °C`
+//     }
+//     document.getElementById('form').reset()
+//    e.preventDefault()
+// });
 
+// farenheit.addEventListener('click', function(e){
+//     // Set the entered number to a variable
+//     number = parseFloat(document.getElementById('temperature').value)
+//     //Check if number
+//     if(isNaN(number)) {
+//         alert('Please enter a valid number');
+//     } else {
+//     // Convert to Farenheit
+//     const fahrenheitConverted = (number*(9/5)+32).toFixed(2);
+//     results.innerHTML = `${fahrenheitConverted} °F`
+//     }
+//     document.getElementById('form').reset()
+// e.preventDefault()
+// });
 
-farenheit.addEventListener('click', function(e){
-    // Set the entered number to a variable
+temperature.onkeyup = function(){
     number = parseFloat(document.getElementById('temperature').value)
-    //Check if number
-    if(isNaN(number)) {
-        alert('Please enter a valid number');
+    const option = document.querySelector('#temp-type').selectedIndex 
+    if (isNaN(number)){
+        results.innerHTML = ""
+    } else if (option == 0) {
+        //number = parseFloat(document.getElementById('temperature').value)
+        //Convert to Farenheit
+        const celsiusConverted = ((number-32)*5/9).toFixed(2);
+        results.innerHTML = `${celsiusConverted} °C`
     } else {
-    // Convert to Farenheit
-    const celsiusConverted = (number*(9/5)+32).toFixed(2);
-    results.innerHTML = `${celsiusConverted} °F`
+        const fahrenheitConverted = (number*(9/5)+32).toFixed(2);
+        results.innerHTML = `${fahrenheitConverted} °F`
     }
-    document.getElementById('form').reset()
-e.preventDefault()
-});
+};
 
+//temperature.onkeyup = converter();
